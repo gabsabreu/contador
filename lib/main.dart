42dc967c8ec4d340b3bd41908970455ffaa4265f
Gabe Abreu de Araujo <g.abreu958@gmail.com>
2021-09-29 11:59:52 -0300

    building app

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+      title: "Contador de Pessoas",
+      home: Container(
+        color: Colors.amber,
+      )));
+}
