80f77d1992427e9fe70c242d9e0ed08c5a29aa96
Gabe Abreu de Araujo <g.abreu958@gmail.com>
2021-09-29 14:02:11 -0300

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,41 @@ import 'package:flutter/material.dart';
 void main(List<String> args) {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Container(
-        color: Colors.amber,
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Pessoas: 0",
+            style: TextStyle(color: Colors.white),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "+1",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "-1",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    )),
+              ),
+            ],
+          ),
+          Text(
+            "Pode Entrar!",
+            style: TextStyle(
+                color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
+          )
+        ],
       )));
 }
