99710d0b97177dbb7d1f3055346ed77c448ae8e1
Gabe Abreu de Araujo <g.abreu958@gmail.com>
2021-09-29 14:22:19 -0300

    add background

--- lib/main.dart
+++ lib/main.dart
@@ -3,41 +3,50 @@ import 'package:flutter/material.dart';
 void main(List<String> args) {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Pessoas: 0",
-            style: TextStyle(color: Colors.white),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "+1",
-                      style: TextStyle(color: Colors.white, fontSize: 40),
-                    )),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "-1",
-                      style: TextStyle(color: Colors.white, fontSize: 40),
-                    )),
-              ),
-            ],
-          ),
-          Text(
-            "Pode Entrar!",
-            style: TextStyle(
-                color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
-          )
-        ],
-      )));
+      home: Stack(children: [
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Pessoas: 0",
+              style: TextStyle(color: Colors.white),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "+1",
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      )),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "-1",
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      )),
+                ),
+              ],
+            ),
+            Text(
+              "Pode Entrar!",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30),
+            )
+          ],
+        )
+      ])));
 }
